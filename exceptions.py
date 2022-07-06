913462a74b2e71ae474e8903bcc64fe0cbb745ac
Philip Kent <philip.kent@nist.gov>
2022-07-06 15:55:52 -0600

    added exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,24 @@
+class RewindAndLoadIon(Exception):
+    pass
+
+
+class IonPresent(Exception):
+    pass
+
+
+class Paused(Exception):
+    """Exception raised on the core device when a scan should either pause and yield to a higher priority experiment or
+    should terminate."""
+    pass
+
+
+class LostIon(Exception):
+    pass
+
+
+class LoadIon(Exception):
+    pass
+
+
+class ExitScan(Exception):
+    pass
